240cf4ecf491367c9b4279be943a64b7fec99cd6
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-25 23:00:14 +0300

    update and delete added

--- class-notes/009_CRUD/fscohort/views.py
+++ class-notes/009_CRUD/fscohort/views.py
@@ -13,6 +13,8 @@ def student_list(request):
     }
     return render(request, 'fscohort/student_list.html', context)
 
+#! Add
+
 def student_add(request):
     form = StudentForm()
     if request.method == 'POST':
@@ -24,3 +26,38 @@ def student_add(request):
         'form' : form
     }
     return render(request, 'fscohort/student_add.html', context)
+
+#! Update
+
+def student_update(request, id):
+    student =Student.objects.get(id=id)
+    form = StudentForm(instance=student)
+    if request.method== "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    context = {
+        'form':form,
+    }
+    return render(request, 'fscohort/student_update.html', context)
+
+
+#! Delete
+
+def student_delete(request, id):
+    student = Student.objects.get(id=id)
+    if request.method =='POST':
+        student.delete()
+        return redirect('list')
+    context = {
+        'student' : student
+    }
+    return render(request, 'fscohort/student_delete.html', context)
+
+def student_detail(request, id):        
+    student = Student.objects.get(id=id)
+    context = {
+        'student': student
+    }
+    return render(request, 'fscohort/student_detail.html', context)
