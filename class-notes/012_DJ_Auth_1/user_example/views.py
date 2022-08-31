0a1f01d4732428b95137baf800794fab137d2fd3
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-08-31 22:29:27 +0300

    authentication

--- class-notes/012_DJ_Auth_1/user_example/views.py
+++ class-notes/012_DJ_Auth_1/user_example/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 
 def home(request):
@@ -6,3 +8,27 @@ def home(request):
 
 def special(request):
     return render(request, "user_example/special.html")
+
+def register(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+
+        user = authenticate(username=username, password= password)
+
+        # login(request, user) #!burayı yazarsak otomatik login olur.bir sonraki satırda da home a yönlendiriyoruz.
+
+        # return redirect("home")
+        return redirect("login")
+
+
+
+
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'registration/register.html', context)
