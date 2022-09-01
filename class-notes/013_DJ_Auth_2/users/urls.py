981f0c8d1f3ff76b99aa5c98c2e2e87480cd266b
akif-onder <100137858+akif-onder@users.noreply.github.com>
2022-09-01 22:17:35 +0300

    continued

--- class-notes/013_DJ_Auth_2/users/urls.py
+++ class-notes/013_DJ_Auth_2/users/urls.py
@@ -1,4 +1,8 @@
+from multiprocessing.util import is_abstract_socket_namespace
 from django.urls import path
+from .views import register, user_logout
 
 urlpatterns = [
+    path('logout/', user_logout, name='user_logout'),
+    path('register/', register, name='register'),
 ]
--- class-notes/013_DJ_Auth_2/users/views.py
+++ class-notes/013_DJ_Auth_2/users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import logout
 
 from django.contrib import messages
 
+from .forms import UserForm, UserProfileForm
+
+
 def home(request):
     return render(request, 'users/home.html')
+
+def user_logout(request):
+    logout(request)
+    return redirect('home')
+
+def register(request):
+    form_user=UserForm()
+    form_profile = UserProfileForm()
+    context={
+        'form_profile':form_profile,
+        'form_user':form_user,
+    }
+
+    return render(request, 'users/register.html', context)
